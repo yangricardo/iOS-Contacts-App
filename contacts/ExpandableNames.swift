058b70b4d8cfcffc75968e293f82bc1ae50b1a1f
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-03-03 01:09:25 -0300

    Expandable and Collapsable rows https://www.youtube.com/watch?v=Q8k9E1gQ_qg&t=13s

---
+++ contacts/ExpandableNames.swift
@@ -0,0 +1,14 @@
+//
+//  ExpandableNames.swift
+//  contacts
+//
+//  Created by Yang Ricardo  on 03/03/2018.
+//  Copyright © 2018 Yang Ricardo . All rights reserved.
+//
+
+import Foundation
+
+struct ExpandableNames {
+    var isExpanded:Bool
+    let names: [String]
+}
--- contacts/ViewController.swift
+++ contacts/ViewController.swift
@@ -13,9 +13,10 @@ class ViewController: UITableViewController {
     let cellID = "cellID"
     
     var namesLists = [
-        ["Yang","Ricardo","Barcellos","Miranda",],
-        ["Vera","Lucia","Alves","Oliveira",],
-        ["Douglas","Peixoto"],
+        ExpandableNames(isExpanded:false,names:["Yang","Ricardo","Barcellos","Miranda",]),
+        ExpandableNames(isExpanded:false,names:["Vera","Lucia","Alves","Oliveira",]),
+
+        ExpandableNames(isExpanded:false,names:["Douglas","Peixoto"]),
     ]
     
     var showIndexPaths = false
@@ -24,7 +25,7 @@ class ViewController: UITableViewController {
         var indexPathReloading = [IndexPath]()
         
         for section in namesLists.indices{
-            for row in namesLists[section].indices{
+            for row in namesLists[section].names.indices{
                 let indexPath = IndexPath(row: row, section: section)
                 indexPathReloading.append(indexPath)
             }
@@ -56,10 +57,47 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let label = UILabel()
-        label.text = "Seção"
-        label.backgroundColor = UIColor.lightGray
-        return label
+        //let label = UILabel()
+        //label.text = "Seção"
+        //label.backgroundColor = UIColor.lightGray
+        //return label
+        
+        let closeButton = UIButton(type: .system)
+        closeButton.setTitle("Close", for: .normal)
+        closeButton.setTitleColor(.black, for: .normal)
+        closeButton.backgroundColor = .yellow
+        closeButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        closeButton.addTarget(self, action: #selector(handleExpandCollapse), for: .touchUpInside)
+        closeButton.tag = section
+        return closeButton
+        
+    }
+    
+    @objc func handleExpandCollapse(button:UIButton){
+        var indexPaths = [IndexPath]()
+        let section = button.tag
+        
+        for row in namesLists[section].names.indices{
+            let indexPath = IndexPath(row: row, section: section)
+            indexPaths.append(indexPath)
+        }
+        
+        let isExpanded = namesLists[section].isExpanded
+        
+        namesLists[section].isExpanded = !isExpanded
+        
+        button.setTitle(isExpanded ? "Abrir" : "Fechar", for: .normal)
+        
+        if isExpanded{
+            tableView.deleteRows(at: indexPaths, with: .fade)
+        } else{
+            tableView.insertRows(at: indexPaths, with: .fade)
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 36
     }
     
     //Numero de Seções
@@ -69,14 +107,14 @@ class ViewController: UITableViewController {
     
     //Numero de Linhas
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return namesLists[section].count
+        return namesLists[section].isExpanded ? namesLists[section].names.count : 0
     }
 
     //Conteudo da Celula
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
         
-        let name = self.namesLists[indexPath.section][indexPath.row]
+        let name = self.namesLists[indexPath.section].names[indexPath.row]
         
         cell.textLabel?.text = !showIndexPaths ? name : "\(name) Seção: \(indexPath.section) Row: \(indexPath.row)";
         
