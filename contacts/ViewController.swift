4db726458e03b530a31b29fd10082cbc7b2fd6f9
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-03-02 23:18:58 -0300

    Criando TableView programaticamente https://www.youtube.com/watch?v=VFtsSEYDNRU

--- contacts/ViewController.swift
+++ contacts/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let cellID = "cellID"
+    
+    var namesLists = [
+        ["Yang","Ricardo","Barcellos","Miranda",],
+        ["Vera","Lucia","Alves","Oliveira",],
+        ["Douglas","Peixoto"],
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Contatos"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Seção"
+        label.backgroundColor = UIColor.lightGray
+        return label
+    }
+    
+    //Numero de Seções
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return namesLists.count
+    }
+    
+    //Numero de Linhas
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return namesLists[section].count
+    }
 
-
+    //Conteudo da Celula
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let name = self.namesLists[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = "\(name) Seção: \(indexPath.section) Row: \(indexPath.row)"
+        
+        return cell
+    }
 }
 
